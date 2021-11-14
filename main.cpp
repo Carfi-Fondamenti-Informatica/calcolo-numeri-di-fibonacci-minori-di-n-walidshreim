4cfbc1b23e7240aaba827af9ea52bffb2361a1db
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-11-14 21:49:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-   return 0;
+   int n,i=0, b = 1, a = 0, c=0;
+        cin >> n;
+        cout << b << endl;
+            for (i = 0; i<(n+1); i++) {
+                if(c<=n) {
+                    cout << c << endl;
+                    a = b;
+                    b = c;
+                    c = a + b;
+                }
+            }
+        return 0;
 }
